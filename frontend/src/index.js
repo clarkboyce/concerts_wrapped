5d11c4447f40fb735c0cb84407127ce821a7dcfa
thisismyamazingusername <111548413+thisismyamazingusername@users.noreply.github.com>
2025-05-04 20:40:51 -0500

    network error now maybe bc of proxy time

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,11 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-if (process.env.NODE_ENV === 'production') {
-  console.log = function () {};
-  console.warn = function () {};
-  console.error = function () {};
-}
+// if (process.env.NODE_ENV === 'production') {
+//   console.log = function () {};
+//   console.warn = function () {};
+//   console.error = function () {};
+// }
 
 root.render(
   <React.StrictMode>
