00f834f998545b413070dac3917cad59ce3ef2e1
thisismyamazingusername <111548413+thisismyamazingusername@users.noreply.github.com>
2025-05-04 21:23:41 -0500

    testing v3

--- frontend/src/components/api/client.js
+++ frontend/src/components/api/client.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? '/data-api'
-  : process.env.REACT_APP_API_URL;
+  ? 'https://concerts-backend-vkh2y6nrxa-uc.a.run.app' || '/data-api'
+  : process.env.REACT_APP_BEND_API_URL;
 
 const apiClient = axios.create({
   baseURL,
-  headers: { 'Content-Type': 'application/json' },
-  // Force HTTPS even if any code tries HTTP
-  transformRequest: [(data, headers) => {
-    if (headers && headers.url && headers.url.startsWith('http://')) {
-      headers.url = headers.url.replace('http://', 'https://');
-    }
-    return data;
-  }]
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default apiClient;
